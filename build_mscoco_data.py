dc132b1863d93b5316438f0bb659e74fb2ecb0c8
Ajaykrishnan Jayagopal <akj1996@gmail.com>
2019-03-29 18:58:17 +0530

    debug

--- build_mscoco_data.py
+++ build_mscoco_data.py
@@ -187,7 +187,9 @@ def _int64_feature(value):
 
 def _bytes_feature(value):
     """Wrapper for inserting a bytes Feature into a SequenceExample proto."""
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value.encode('utf-8')]))
+    if type(value) == str:
+        value = value.encode('utf-8')
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
 def _int64_feature_list(values):
