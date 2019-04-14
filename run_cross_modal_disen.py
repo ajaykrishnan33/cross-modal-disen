4848a1d362af722e0a486db2eb34381532707d7e
Ajaykrishnan Jayagopal <akj1996@gmail.com>
2019-04-14 22:19:44 +0530

    real train dataset

--- run_cross_modal_disen.py
+++ run_cross_modal_disen.py
@@ -29,7 +29,7 @@ def load_examples():
         raise Exception("input_dir does not exist")
 
     if config.mode == "train":
-        train_dataset = MSCOCODataset("val")
+        train_dataset = MSCOCODataset("train")
         ids, inputsI, inputsT = train_dataset.next_batch()
         count = train_dataset.total_size
         steps_per_epoch = int(math.ceil(train_dataset.total_size/config.batch_size))
@@ -220,50 +220,50 @@ def main():
         return convert_index_txt(index_vectors)
 
     # reverse any processing on images and text so they can be written to disk or displayed to user
-    # with tf.name_scope("convert_inputsI"):
-    #     converted_inputsI = convert_img(inputsI)
+    with tf.name_scope("convert_inputsI"):
+        converted_inputsI = convert_img(inputsI)
 
-    # with tf.name_scope("convert_inputsT"):
-    #     converted_inputsT = convert_index_txt(inputsT)
+    with tf.name_scope("convert_inputsT"):
+        converted_inputsT = convert_index_txt(inputsT)
 
-    # with tf.name_scope("convert_outputsI2T"):
-    #     converted_outputsI2T = convert_txt(outputsI2T)
+    with tf.name_scope("convert_outputsI2T"):
+        converted_outputsI2T = convert_txt(outputsI2T)
 
-    # with tf.name_scope("convert_outputsT2I"):
-    #     converted_outputsT2I = convert_img(outputsT2I)
+    with tf.name_scope("convert_outputsT2I"):
+        converted_outputsT2I = convert_img(outputsT2I)
 
-    # with tf.name_scope("convert_outputsI2Tp"):
-    #     converted_outputsI2Tp = convert_txt(outputsI2Tp)
+    with tf.name_scope("convert_outputsI2Tp"):
+        converted_outputsI2Tp = convert_txt(outputsI2Tp)
 
-    # with tf.name_scope("convert_outputsT2Ip"):
-    #     converted_outputsT2Ip = convert_img(outputsT2Ip)
+    with tf.name_scope("convert_outputsT2Ip"):
+        converted_outputsT2Ip = convert_img(outputsT2Ip)
 
-    # with tf.name_scope("convert_outputs_exclusiveI2T"):
-    #     converted_outputs_exclusiveI2T = convert_txt(outputs_exclusiveI2T)
+    with tf.name_scope("convert_outputs_exclusiveI2T"):
+        converted_outputs_exclusiveI2T = convert_txt(outputs_exclusiveI2T)
 
-    # with tf.name_scope("convert_outputs_exclusiveT2I"):
-    #     converted_outputs_exclusiveT2I = convert_img(outputs_exclusiveT2I)
+    with tf.name_scope("convert_outputs_exclusiveT2I"):
+        converted_outputs_exclusiveT2I = convert_img(outputs_exclusiveT2I)
 
-    # with tf.name_scope("convert_auto_outputsI"):
-    #     converted_auto_outputI = convert_img(auto_outputI)
+    with tf.name_scope("convert_auto_outputsI"):
+        converted_auto_outputI = convert_img(auto_outputI)
 
-    # with tf.name_scope("convert_auto_outputsT"):
-    #     converted_auto_outputT = convert_txt(auto_outputT)
+    with tf.name_scope("convert_auto_outputsT"):
+        converted_auto_outputT = convert_txt(auto_outputT)
 
-    # with tf.name_scope("encode_data"):
-    #     display_fetches = {
-    #         "ids": examples.ids,
-    #         "inputsI": tf.map_fn(tf.image.encode_png, converted_inputsI, dtype=tf.string, name="inputI_pngs"),
-    #         "inputsT": converted_inputsT,
-    #         "outputsI2T": converted_outputsI2T,
-    #         "outputsT2I": tf.map_fn(tf.image.encode_png, converted_outputsT2I, dtype=tf.string, name="outputT2I_pngs"),
-    #         "outputsI2Tp": converted_outputsI2Tp,
-    #         "outputsT2Ip": tf.map_fn(tf.image.encode_png, converted_outputsT2Ip, dtype=tf.string, name="outputT2Ip_pngs"),
-    #         "outputs_exclusiveI2T": converted_outputs_exclusiveI2T,
-    #         "outputs_exclusiveT2I": tf.map_fn(tf.image.encode_png, converted_outputs_exclusiveT2I, dtype=tf.string, name="output_exclusiveT2I_pngs"),
-    #         "auto_outputsI": tf.map_fn(tf.image.encode_png, converted_auto_outputI, dtype=tf.string, name="auto_outputI_pngs"),
-    #         "auto_outputsT": converted_auto_outputT
-    #     }
+    with tf.name_scope("encode_data"):
+        display_fetches = {
+            "ids": examples.ids,
+            "inputsI": tf.map_fn(tf.image.encode_png, converted_inputsI, dtype=tf.string, name="inputI_pngs"),
+            "inputsT": converted_inputsT,
+            "outputsI2T": converted_outputsI2T,
+            "outputsT2I": tf.map_fn(tf.image.encode_png, converted_outputsT2I, dtype=tf.string, name="outputT2I_pngs"),
+            "outputsI2Tp": converted_outputsI2Tp,
+            "outputsT2Ip": tf.map_fn(tf.image.encode_png, converted_outputsT2Ip, dtype=tf.string, name="outputT2Ip_pngs"),
+            "outputs_exclusiveI2T": converted_outputs_exclusiveI2T,
+            "outputs_exclusiveT2I": tf.map_fn(tf.image.encode_png, converted_outputs_exclusiveT2I, dtype=tf.string, name="output_exclusiveT2I_pngs"),
+            "auto_outputsI": tf.map_fn(tf.image.encode_png, converted_auto_outputI, dtype=tf.string, name="auto_outputI_pngs"),
+            "auto_outputsT": converted_auto_outputT
+        }
 
     # with tf.name_scope("extract_features"):
     #     features_fetches = {
@@ -276,54 +276,55 @@ def main():
     #         "eR_T2I": eR_T2I,
     #     }
 
-    # summaries
-    # with tf.name_scope("I1_input_summary"):
-    #     tf.summary.image("inputsI", converted_inputsI,max_outputs=3)
+    summaries
+    with tf.name_scope("I1_input_summary"):
+        tf.summary.image("inputsI", converted_inputsI,max_outputs=3)
 
-    # with tf.name_scope("T1_input_summary"):
-    #     tf.summary.text("inputsT", converted_inputsT)
+    with tf.name_scope("T1_input_summary"):
+        tf.summary.text("inputsT", converted_inputsT)
 
-    # with tf.name_scope("I2T_output_summary"):
-    #     tf.summary.text("outputsI2T", converted_outputsI2T)
+    with tf.name_scope("I2T_output_summary"):
+        tf.summary.text("outputsI2T", converted_outputsI2T)
 
-    # with tf.name_scope("T2I_output_summary"):
-    #     tf.summary.image("outputsT2I", converted_outputsT2I,max_outputs=3)
+    with tf.name_scope("T2I_output_summary"):
+        tf.summary.image("outputsT2I", converted_outputsT2I,max_outputs=3)
 
-    # with tf.name_scope("I_autoencoder_summary"):
-    #     tf.summary.image("auto_outputI", converted_auto_outputI,max_outputs=3)
+    with tf.name_scope("I_autoencoder_summary"):
+        tf.summary.image("auto_outputI", converted_auto_outputI,max_outputs=3)
 
-    # with tf.name_scope("T_autoencoder_summary"):
-    #     tf.summary.text("auto_outputT", converted_auto_outputT)
+    with tf.name_scope("T_autoencoder_summary"):
+        tf.summary.text("auto_outputT", converted_auto_outputT)
 
-    # with tf.name_scope("otherNoise_output_summary"):
-    #     tf.summary.text("outputsI2Tp", converted_outputsI2Tp)
-    #     tf.summary.image("outputsT2Ip", converted_outputsT2Ip,max_outputs=3)
+    with tf.name_scope("otherNoise_output_summary"):
+        tf.summary.text("outputsI2Tp", converted_outputsI2Tp)
+        tf.summary.image("outputsT2Ip", converted_outputsT2Ip,max_outputs=3)
 
-    # with tf.name_scope("zzexclusive_I2T_summary"):
-    #     tf.summary.text("outputsI2T", converted_outputs_exclusiveI2T)
+    with tf.name_scope("zzexclusive_I2T_summary"):
+        tf.summary.text("outputsI2T", converted_outputs_exclusiveI2T)
 
-    # with tf.name_scope("zzexclusive_T2I_summary"):
-    #     tf.summary.image("outputsT2I", converted_outputs_exclusiveT2I,max_outputs=3)
+    with tf.name_scope("zzexclusive_T2I_summary"):
+        tf.summary.image("outputsT2I", converted_outputs_exclusiveT2I,max_outputs=3)
 
-    # tf.summary.scalar("discriminatorI2T_loss", model.discrimI2T_loss)
-    # tf.summary.scalar("discriminatorT2I_loss", model.discrimT2I_loss)
-    # tf.summary.scalar("generatorI2T_loss", model.genI2T_loss)
-    # tf.summary.scalar("generatorT2I_loss", model.genT2I_loss)
-    # tf.summary.scalar("generator_exclusiveI2T_loss", model.gen_exclusiveI2T_loss)
-    # tf.summary.scalar("discriminator_exclusiveI2T_loss", model.discrim_exclusiveI2T_loss)
-    # tf.summary.scalar("generator_exclusiveT2I_loss", model.gen_exclusiveT2I_loss)
-    # tf.summary.scalar("discriminator_exclusiveT2I_loss", model.discrim_exclusiveT2I_loss)
-    # tf.summary.scalar("autoencoderI_loss", model.autoencoderI_loss)
-    # tf.summary.scalar("autoencoderT_loss", model.autoencoderT_loss)
-    # tf.summary.scalar("feat_recon_loss", model.feat_recon_loss)
-    # tf.summary.scalar("code_sR_I2T_recon_loss", model.code_sR_I2T_recon_loss)
-    # tf.summary.scalar("code_sR_T2I_recon_loss", model.code_sR_T2I_recon_loss)
-    # tf.summary.scalar("code_eR_I2T_recon_loss", model.code_eR_I2T_recon_loss)
-    # tf.summary.scalar("code_eR_T2I_recon_loss", model.code_eR_T2I_recon_loss)
-    # tf.summary.scalar("code_recon_loss", model.code_recon_loss)
+    tf.summary.scalar("discriminatorI2T_loss", model.discrimI2T_loss)
+    tf.summary.scalar("discriminatorT2I_loss", model.discrimT2I_loss)
+    tf.summary.scalar("generatorI2T_loss", model.genI2T_loss)
+    tf.summary.scalar("generatorT2I_loss", model.genT2I_loss)
+    tf.summary.scalar("generator_exclusiveI2T_loss", model.gen_exclusiveI2T_loss)
+    tf.summary.scalar("discriminator_exclusiveI2T_loss", model.discrim_exclusiveI2T_loss)
+    tf.summary.scalar("generator_exclusiveT2I_loss", model.gen_exclusiveT2I_loss)
+    tf.summary.scalar("discriminator_exclusiveT2I_loss", model.discrim_exclusiveT2I_loss)
+    tf.summary.scalar("autoencoderI_loss", model.autoencoderI_loss)
+    tf.summary.scalar("autoencoderT_loss", model.autoencoderT_loss)
+    tf.summary.scalar("feat_recon_loss", model.feat_recon_loss)
+    tf.summary.scalar("code_sR_I2T_recon_loss", model.code_sR_I2T_recon_loss)
+    tf.summary.scalar("code_sR_T2I_recon_loss", model.code_sR_T2I_recon_loss)
+    tf.summary.scalar("code_eR_I2T_recon_loss", model.code_eR_I2T_recon_loss)
+    tf.summary.scalar("code_eR_T2I_recon_loss", model.code_eR_T2I_recon_loss)
+    tf.summary.scalar("code_recon_loss", model.code_recon_loss)
 
-    # with tf.name_scope("parameter_count"):
-    #     parameter_count = tf.reduce_sum([tf.reduce_prod(tf.shape(v)) for v in tf.trainable_variables()])
+    with tf.name_scope("parameter_count"):
+
+        parameter_count = tf.reduce_sum([tf.reduce_prod(tf.shape(v)) for v in tf.trainable_variables()])
 
     saver = tf.train.Saver(max_to_keep=1)
 
@@ -332,7 +333,7 @@ def main():
     sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
     sess_config.gpu_options.allow_growth = True
     with sv.managed_session(config=sess_config) as sess:
-        # print("parameter_count =", sess.run(parameter_count))
+        print("parameter_count =", sess.run(parameter_count))
         print("Started session")
         if config.checkpoint is not None:
             print("loading model from checkpoint")
@@ -400,8 +401,8 @@ def main():
                 if should(config.summary_freq):
                     fetches["summary"] = sv.summary_op
 
-                # if should(config.display_freq):
-                #     fetches["display"] = display_fetches
+                if should(config.display_freq):
+                    fetches["display"] = display_fetches
 
                 results = sess.run(fetches, options=options, run_metadata=run_metadata)
 
